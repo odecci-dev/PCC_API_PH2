44904b83135268705fb5ef552cf0980abf460ccb
Nestor Valleser <nestor.valleser@odecci.com>
2025-04-05 21:10:32 +0800

    Animals Update

--- API-PCC/Controllers/BuffAnimalsController.cs
+++ API-PCC/Controllers/BuffAnimalsController.cs
@@ -270,7 +270,7 @@ namespace API_PCC.Controllers
                                    combined => combined.herd.Center,
                                    center => center.Id,
                                    (combined, center) => new { combined.animal, center })
-                             .Where(result => result.center.Id== searchFilter.centerid)
+                             .Where(result => result.center.Id == searchFilter.centerid)
                              .Select(result => result.animal);
 
             //if (!searchFilter.userid.IsNullOrEmpty())
